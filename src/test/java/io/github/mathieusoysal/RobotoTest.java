17d798a0f0553d1af244db9df43f4f00aad808e3
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-03-29 16:43:44 +0000

    Add not validate test to Roboto

--- src/test/java/io/github/mathieusoysal/RobotoTest.java
+++ src/test/java/io/github/mathieusoysal/RobotoTest.java
@@ -1,11 +1,15 @@
 package io.github.mathieusoysal;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import com.microsoft.playwright.Page;
+
+import io.github.mathieusoysal.exceptions.EmailFieldNotFoundException;
+import io.github.mathieusoysal.exceptions.PasswordFieldNotFoundException;
 
 class RobotoTest {
 
@@ -36,6 +40,24 @@ class RobotoTest {
         });
         Page page = roboto.getPage();
         assertThat(page.locator("input[type='password']").first()).hasValue("password");
+        roboto.close();
+    }
+
+    @Test
+    void emailConnection_witWrongURL_test() {
+        Roboto roboto = new Roboto("https://playwright.dev");
+        assertThrows(EmailFieldNotFoundException.class, () -> {
+            roboto.fillEmailField("email");
+        });
+        roboto.close();
+    }
+
+    @Test
+    void passwordConnection_witWrongURL_test() {
+        Roboto roboto = new Roboto("https://playwright.dev");
+        assertThrows(PasswordFieldNotFoundException.class, () -> {
+            roboto.fillPasswordField("password");
+        });
         roboto.close();
     }
 
