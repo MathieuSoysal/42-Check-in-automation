7235b59f13057b8519a6ffbf411f8fc8f376cf8d
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-04-24 20:43:17 +0000

    Validation tests

--- src/main/java/io/github/mathieusoysal/App.java
+++ src/main/java/io/github/mathieusoysal/App.java
@@ -1,10 +1,21 @@
 package io.github.mathieusoysal;
 
+import com.github.forax.beautifullogger.Logger;
+
+import io.github.mathieusoysal.exceptions.EnvironementVariableNotFoundException;
 import io.github.mathieusoysal.exceptions.RobotoException;
 
 public class App {
 
+    private static final Logger LOGGER = Logger.getLogger();
+
     public static void main(String[] args) {
+        try {
+            checkEnvVariable("TEST_EMAIL");
+            checkEnvVariable("TEST_PASSWORD");
+        } catch (EnvironementVariableNotFoundException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
         try (Roboto roboto = new Roboto()) {
             roboto.connect(System.getenv("TEST_EMAIL"), System.getenv("TEST_PASSWORD"));
             while (!roboto.checkinButtonIsPresent())
@@ -12,6 +23,12 @@ public class App {
             roboto.clickOnSubscription();
         } catch (RobotoException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    static void checkEnvVariable(String name) throws EnvironementVariableNotFoundException {
+        if (System.getenv(name) == null) {
+            throw new EnvironementVariableNotFoundException(name);
         }
     }
 
