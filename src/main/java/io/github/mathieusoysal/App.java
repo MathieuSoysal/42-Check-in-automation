1f968c627a362ffb5240293f5f31cc2d24741f3e
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-04-28 23:23:22 +0000

    Update app

--- src/main/java/io/github/mathieusoysal/App.java
+++ src/main/java/io/github/mathieusoysal/App.java
@@ -12,21 +12,27 @@ public class App {
     private static final String ENV_NAME_PASSWORD = "TEST_PASSWORD";
     private static final String ENV_NAME_MAIL = "TEST_EMAIL";
     private static final Logger LOGGER = Logger.getLogger();
+    private static double startTime;
 
     public static void main(String[] args) {
         try (Roboto roboto = new Roboto()) {
             checkAllEnvVariables(ENV_NAME_MAIL, ENV_NAME_PASSWORD);
             roboto.connect(System.getenv(ENV_NAME_MAIL), System.getenv(ENV_NAME_PASSWORD));
             roboto.refreshPage();
-            while (!roboto.checkinButtonIsPresent())
+            while (!roboto.checkinButtonIsPresent() && !fiftenMinutesPassed())
                 roboto.refreshPage();
-            roboto.clickOnSubscription();
         } catch (RobotoException e) {
             System.out.println(e.getMessage());
         } catch (EnvironementVariableNotFoundException e) {
             LOGGER.error(e.getMessage(), e);
             System.exit(1);
         }
+    }
+
+    static boolean fiftenMinutesPassed() {
+        if (startTime == 0)
+            startTime = System.currentTimeMillis();
+        return System.currentTimeMillis() - startTime > 15 * 60 * 1000;
     }
 
     static void checkAllEnvVariables(String... envNames) throws EnvironementVariableNotFoundException {
